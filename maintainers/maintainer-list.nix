46942ac2a4a375b05090114135f28e8b2ff4b487
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-02-23 21:27:11 -0500

    maintainers: add lopsided98

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2041,6 +2041,11 @@
     github = "lo1tuma";
     name = "Mathias Schreck";
   };
+  lopsided98 = {
+    email = "benwolsieffer@gmail.com";
+    github = "lopsided98";
+    name = "Ben Wolsieffer";
+  };
   loskutov = {
     email = "ignat.loskutov@gmail.com";
     github = "loskutov";
